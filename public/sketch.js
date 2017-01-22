fb8eb02f830dc7590c5dd1cbf27e342ce89d609b
kouta2 <arvind.kouta@gmail.com>
2017-01-22 13:20:00 -0600

    Eating Mechanics established
    Server to client communication of locations of random blobs and client
    blobs is set up along with eating mecahnics.

--- public/sketch.js
+++ public/sketch.js
@@ -1,30 +1,22 @@
 var client_blobs = {}; // list of client blobs locations and size
 var random_blobs = {}; // list of random blob locations and size
 var my_blob; // my blob data
-var zoom; 
+var zoom; // used for zooming as my_blob resizes
 var init_radius = 64; // spawn radius of my blob
 
-var socket;
+var socket; // file descriptor to communicate with the server
 
 var p5_agar = function(sketch)
 {
 	sketch.setup = function()
 	{
 		socket = io.connect('http://localhost:3000');
-		// console.log("" + socket);
 
 		var canvas = sketch.createCanvas(700, 550);
 		canvas.parent('agar');
 
 		sketch.translate(sketch.width / 2, sketch.height / 2);
 		my_blob = new Blob(sketch.width / 2, sketch.height / 2, init_radius, sketch);
-
-/*
-		for(var i = 0; i < num_initial_blobs; i++)
-		{
-			blobs.push(new Blob(sketch.random(sketch.width), sketch.random(sketch.height), baby_blob_size, sketch));
-		}
-*/
 
 		var data = {
 			x: my_blob.location.x,
@@ -47,23 +39,12 @@ var p5_agar = function(sketch)
 		zoom = sketch.lerp(zoom, new_zoom, .1);
 		sketch.scale(zoom);
 		sketch.translate(-my_blob.location.x, -my_blob.location.y);
-/*
-		if(sketch.frameCount % 50 == 0)
-		{
-			for(var i = 0; i < num_new_blobs; i++)
-			{
-				blobs.push(new Blob(sketch.random(my_blob.location.x - sketch.width, my_blob.location.x + sketch.width), sketch.random(my_blob.location.y - sketch.height, my_blob.location.y + sketch.height), baby_blob_size, sketch));
-			}
-		}
-*/
 
 		for(var key in client_blobs)
 		{
-			// client_blobs[i].show(sketch);
 			var blob = client_blobs[key];
 			if(key != socket.id)
 			{
-				// console.log("this is me");
 				draw_other_blobs(sketch, blob);
 				if(my_blob.consumes(sketch, blob))
 				{
@@ -73,16 +54,14 @@ var p5_agar = function(sketch)
 			}
 		}
 
-		for(var blob in random_blobs)
+		for(var key in random_blobs)
 		{
-			// var blob = random_blobs[i];
-			// console.log("am I in here?");
+			var blob = random_blobs[key];
 			draw_other_blobs(sketch, blob);
 			if(my_blob.consumes(sketch, blob))
 			{
-				// socket.emit('consumed_random', blob);
-				// random_blobs.delete(blob);
-				delete random_blobs[blob];
+				delete random_blobs[key];
+				socket.emit('consumed_random', key);
 			}
 		}
 
@@ -97,13 +76,11 @@ var p5_agar = function(sketch)
 		};
 		socket.emit('update', my_b);
 
-		// console.log("x: " + my_blob.location.x + " y: " + my_blob.location.y);
 	};
 };
 
 function draw_other_blobs(sketch, blob)
 {
-	// console.log("x: " + blob.x + " y: " + blob.y + " r: " + blob.r);
 	sketch.fill(0, 255, 0);
 	var d = blob.r * 2;
 	sketch.ellipse(blob.x, blob.y, d, d);
--- server.js
+++ server.js
@@ -11,10 +11,11 @@ var io = socket(server);
 var client_blobs = {}; // maps socket id to blob object
 var random_blobs = {}; // set of random blobs
 var num_new_spawns = 10; // 10 new spawns every 25 ms per client
-var random_x = 500; // spawn randoms within 500 pixels
-var random_y = 500; // spawn randoms within 500 pixels
+var random_x = 1000; // spawn randoms within 500 pixels
+var random_y = 1000; // spawn randoms within 500 pixels
 var random_r = 8; // randoms radius is 8
 var num_clients = 0; // number of clients
+var num_randoms = 0; // number of randoms
 
 io.sockets.on('connection', createConnection);
 
@@ -37,6 +38,13 @@ function createConnection(socket)
 		client_blobs[socket.id] = data;
 		socket.emit('other_client_blobs', client_blobs);
 		num_clients++;
+		/*
+		random_blobs["blah"] = {
+			x: data.x + 50,
+			y: data.y + 50,
+			r: random_r
+		};
+		*/
 		// console.log(client_blobs);
 	}
 
@@ -54,23 +62,25 @@ function createConnection(socket)
 
 	function handleConsumingClient(data)
 	{
+		delete client_blobs[data];
+		// num_clients--;
 		socket.disconnect();
 	}
 
 	function handleConsumingRandom(data)
 	{
-		console.log(random_blobs.size);
-		random_blobs.delete(data);
-		console.log(random_blobs.size + "\n");
+		delete random_blobs[data];
+		num_randoms--;
 	}
 
 	function update_all_clients()
 	{
 		// console.log(client_blobs);
 		io.sockets.emit('client_update', client_blobs); // emit where all the other clients are
+
 		
 		// create new randoms based on the locations of the clients
-		if(Object.keys(random_blobs).length <= num_clients * 1000)
+		if(num_randoms <= num_clients * 25)
 		{
 			for(var key in client_blobs)
 			{
@@ -78,22 +88,25 @@ function createConnection(socket)
 				for(var i = 0; i < num_new_spawns; i++)
 				{
 					var data = {
-						x: (Math.random() * random_x) - (random_x / 2) + val.x /*600*/,
-						y: (Math.random() * random_y) - (random_y / 2) + val.y /*600*/,
+						x: (Math.random() * random_x) - (random_x / 2) + val.x,
+						y: (Math.random() * random_y) - (random_y / 2) + val.y,
 						r: random_r
 					};
 					/// console.log("x: " + data.x + " y: " + data.y + " r: " + data.r);
-					random_blobs[data] = data;
+					random_blobs["" + data.x + " " + data.y] = data;
 					// console.log(data.x);
 					// console.log(random_blobs[data]);
 					// console.log(Math.random());
 				}
-				console.log(random_blobs);
+				// console.log(random_blobs);
+				num_randoms += num_new_spawns;
 			}
 		}
+		
 		// emit locations of randoms
 		// print(random_blobs);
 		// console.log(typeof(random_blobs));
+		// console.log(random_blobs);
 		io.sockets.emit('random_update', random_blobs);
 	}
 }
