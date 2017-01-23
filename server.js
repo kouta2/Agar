8f5a8706d3d18c48b9f8675121a83471d308ab53
kouta2 <arvind.kouta@gmail.com>
2017-01-22 19:50:31 -0600

    Cleaned up Code a Bit

--- server.js
+++ server.js
@@ -17,6 +17,37 @@ var random_r = 8; // randoms radius is 8
 var num_clients = 0; // number of clients
 var num_randoms = 0; // number of randoms
 
+var special_num_to_regulate_randoms = 10;
+
+setInterval(update_all_clients, 25);
+
+function update_all_clients()
+{
+	// console.log(client_blobs);
+	io.sockets.emit('client_update', client_blobs); // emit where all the other clients are
+	
+	// create new randoms based on the locations of the clients
+	if(num_randoms <= num_clients * 25)
+	{
+		for(var key in client_blobs)
+		{
+			var val = client_blobs[key];
+			for(var i = 0; i < num_new_spawns; i++)
+			{
+				var data = {
+					x: (Math.random() * random_x) - (random_x / 2) + val.x,
+					y: (Math.random() * random_y) - (random_y / 2) + val.y,
+					r: random_r
+				};
+				random_blobs["" + data.x + " " + data.y] = data;
+			}
+			num_randoms += num_new_spawns;
+		}
+	}
+
+	io.sockets.emit('random_update', random_blobs);
+}
+
 io.sockets.on('connection', createConnection);
 
 function createConnection(socket)
@@ -29,8 +60,6 @@ function createConnection(socket)
 	socket.on('update', handleUpdate);
 	socket.on('consumed_client', handleConsumingClient);
 	socket.on('consumed_random', handleConsumingRandom);
-	setInterval(update_all_clients, 25);
-
 
 	function newClient(data)
 	{
@@ -38,14 +67,6 @@ function createConnection(socket)
 		client_blobs[socket.id] = data;
 		socket.emit('other_client_blobs', client_blobs);
 		num_clients++;
-		/*
-		random_blobs["blah"] = {
-			x: data.x + 50,
-			y: data.y + 50,
-			r: random_r
-		};
-		*/
-		// console.log(client_blobs);
 	}
 
 	function handleDisconnection()
@@ -53,6 +74,7 @@ function createConnection(socket)
 		console.log(socket.id + " disconnected");
 		delete client_blobs[socket.id];
 		num_clients--;
+		socket.emit()
 	}
 
 	function handleUpdate(data)
@@ -71,51 +93,5 @@ function createConnection(socket)
 	{
 		delete random_blobs[data];
 		num_randoms--;
-	}
-
-	function update_all_clients()
-	{
-		// console.log(client_blobs);
-		io.sockets.emit('client_update', client_blobs); // emit where all the other clients are
-
-		
-		// create new randoms based on the locations of the clients
-		if(num_randoms <= num_clients * 25)
-		{
-			for(var key in client_blobs)
-			{
-				var val = client_blobs[key];
-				for(var i = 0; i < num_new_spawns; i++)
-				{
-					var data = {
-						x: (Math.random() * random_x) - (random_x / 2) + val.x,
-						y: (Math.random() * random_y) - (random_y / 2) + val.y,
-						r: random_r
-					};
-					/// console.log("x: " + data.x + " y: " + data.y + " r: " + data.r);
-					random_blobs["" + data.x + " " + data.y] = data;
-					// console.log(data.x);
-					// console.log(random_blobs[data]);
-					// console.log(Math.random());
-				}
-				// console.log(random_blobs);
-				num_randoms += num_new_spawns;
-			}
-		}
-		
-		// emit locations of randoms
-		// print(random_blobs);
-		// console.log(typeof(random_blobs));
-		// console.log(random_blobs);
-		io.sockets.emit('random_update', random_blobs);
-	}
-}
-
-function print(list)
-{
-	console.log('printing');
-	for(let item of list)
-	{
-		console.log(item);
 	}
 }
